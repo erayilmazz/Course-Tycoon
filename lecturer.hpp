2b5504b52f5b5d31b9c775ff70ab26999a3cb99e
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-20 23:10:37 +0300

    lecturer.hpp created.

---
+++ lecturer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+using std::vector;
+
+#include "teacher.hpp"
+
+class Lecturer : public Teacher{
+private:
+    vector<Lesson> lessons;
+public:
+    Lecturer(const string& name, const unordered_map<string,int>& skills);
+    void addLesson(const Lesson& lesson);
+    void displayLessons() const;
+};
+
