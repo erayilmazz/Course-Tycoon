8a4491ee135a8e3bf2a75b887dc2505dcc1b1f5f
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-20 19:54:49 +0300

    book cpp file created.

---
+++ book.cpp
@@ -0,0 +1,10 @@
+#include "book.hpp";
+
+Book::Book(const string& title, const string& topic)
+    :title(title), topic(topic){}
+
+string Book::getTitle() const {return title;}
+string Book::getTopic() const {return topic;}
+
+
+
--- book.hpp
+++ book.hpp
@@ -4,10 +4,10 @@ using std::string;
 
 class Book{
     protected:
-        string name;
-        string subject;
+        string title;
+        string topic;
     public:
-        Book(const string& name, const string& subject);
-        string getName() const;
-        string getSubject() const;
+        Book(const string& title, const string& topic);
+        string getTitle() const;
+        string getTopic() const;
 };
