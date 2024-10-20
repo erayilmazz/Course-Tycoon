f8f2632fd7cb124f469180b849d869b3c2590f51
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-20 23:52:09 +0300

    lesson.cpp

---
+++ lesson.cpp
@@ -0,0 +1,10 @@
+#include "lesson.hpp"
+
+Lesson::Lesson(const string& subject, const Lecturer& lecturer)
+        :subject(subject), lecturer(lecturer) {}
+
+string Lesson::getSubjectName() const {return subject;}
+string Lesson::getLecturerName() const {return lecturer.getName();}
+
+
+
