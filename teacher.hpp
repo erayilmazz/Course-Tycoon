c52e60fd18ac7fd71ecc06ed0666503034266457
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-20 17:09:31 +0300

    comments added.

--- teacher.hpp
+++ teacher.hpp
@@ -8,15 +8,15 @@ using std::unordered_map;
 class Teacher{
 protected:
     string name;
-    unordered_map<string, int> skills;
+    unordered_map<string, int> skills; //dictionary for teacher's skills for each lessons.
 
 public:
     Teacher(const string& name, const unordered_map<string, int>& skills);
     virtual ~Teacher() = default;
 
     string get_name() const;
-    int get_skill(const string& skill) const;
-    unordered_map<string,int> get_skills() const;
+    int get_skill(const string& skill) const; //get specific skill.
+    unordered_map<string,int> get_skills() const; //get all skills.
 }
 
     
