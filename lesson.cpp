e27e8da946f85adf6183a67d525ba3e2810d8fa9
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-24 17:18:34 +0300

    Change lesson funcs to more effective.

--- lesson.cpp
+++ lesson.cpp
@@ -1,10 +1,17 @@
 #include "lesson.hpp"
 
-Lesson::Lesson(const string& subject, const Lecturer& lecturer)
-        :subject(subject), lecturer(lecturer) {}
+Lesson::Lesson(const string& subject)
+        :subject(subject) {}
 
 string Lesson::getSubjectName() const {return subject;}
-string Lesson::getLecturerName() const {return lecturer.getName();}
 
 
 
+void Lesson::setLecturer(Lecturer* lessonLecturer) {lecturer = lessonLecturer;}
+void Lesson::setBook(Book* lessonBook) {book = lessonBook;}
+void Lesson::setClassroom(Classroom* lessonClassroom) {classroom = lessonClassroom;}
+
+
+Lecturer* Lesson::getLecturer() const {return lecturer;}
+Book* Lesson::getBook() const {return book;}
+Classroom* Lesson::getClassroom() const {return classroom;}
--- lesson.hpp
+++ lesson.hpp
@@ -1,15 +1,26 @@
 #pragma once
 #include "lecturer.hpp"
+#include "classroom.hpp"
+#include "book.hpp"
 #include <string>
 using std::string;
 
 class Lesson{
 private:
     string subject;
-    Lecturer lecturer;
+    Lecturer* lecturer;
+    Book* book;
+    Classroom* classroom; 
 
 public:
-    Lesson(const string& subject, const Lecturer& lecturer);
+    Lesson(const string& subject);
     string getSubjectName() const;
-    string getLecturerName() const;
+
+    void setLecturer(Lecturer* lessonLecturer);
+    void setBook(Book* lessonBook);
+    void setClassroom(Classroom* lessonClassroom);
+
+    Lecturer* getLecturer() const;
+    Book* getBook() const;
+    Classroom* getClassroom() const;
 };
