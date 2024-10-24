32060f28ba27679dbc0bd05b9cacacbaba844f41
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-24 16:46:18 +0300

    setter functions add to book.

--- book.cpp
+++ book.cpp
@@ -3,7 +3,9 @@
 Book::Book(const string& title, const string& topic)
     :title(title), topic(topic){}
 
+void Book::setTitle(string titleName) {title = titleName;}
 string Book::getTitle() const {return title;}
+void Book::setTopic(string topicName) {topic = topicName;}
 string Book::getTopic() const {return topic;}
 
 
--- book.hpp
+++ book.hpp
@@ -9,6 +9,9 @@ class Book{
     public:
         Book() = default;
         Book(const string& title, const string& topic);
+
+        void setTitle(string titleName);
         string getTitle() const;
+        void setTopic(string topicName);
         string getTopic() const;
 };
