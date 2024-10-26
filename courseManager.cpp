ee5effcf7fd26d131aea96a481d81a721f9e1897
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-26 11:47:48 +0300

    courseManager created.

--- courseManager.cpp
+++ courseManager.cpp
@@ -2,22 +2,10 @@
 
 #include "courseManager.hpp"
 
-void CourseManager::addTeacher(Teacher* teacher) {
-    teachers.push_back(teacher);
-}
-void CourseManager::addAuthor(Author* author) {
-    authors.push_back(author);
-}
-void CourseManager::addBook(Book* book) {
-    books.push_back(book);
-}
-void CourseManager::addLecturer(Lecturer* lecturer) {
-    lecturers.push_back(lecturer);
-}
-void CourseManager::addLesson(Lesson* lesson) {
-    lessons.push_back(lesson);
-}
-void CourseManager::addClassroom(Classroom* classroom) {
-    classrooms.push_back(classroom);
-}
+void CourseManager::addTeacher(Teacher* teacher) {teachers.push_back(teacher);}
+void CourseManager::addAuthor(Author* author) {authors.push_back(author);}
+void CourseManager::addBook(Book* book) {books.push_back(book);}
+void CourseManager::addLecturer(Lecturer* lecturer) {lecturers.push_back(lecturer);}
+void CourseManager::addLesson(Lesson* lesson) {lessons.push_back(lesson);}
+void CourseManager::addClassroom(Classroom* classroom) {classrooms.push_back(classroom);}
 
