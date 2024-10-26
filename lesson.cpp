21bb1617c5e83fea3aa22c5af64095d039ea600c
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-26 15:04:52 +0300

    add name.

--- lesson.cpp
+++ lesson.cpp
@@ -1,17 +1,17 @@
 #include "lesson.hpp"
 #include "classroom.hpp"
-Lesson::Lesson(const string& subject)
-        :subject(subject) {}
+Lesson::Lesson(const string& name)
+        :name(name) {}
 
-string Lesson::getSubjectName() const {return subject;}
+string Lesson::getName() const {return name;}
 
 
-
+void Lesson::setSubject(string lessonSubject){subject = lessonSubject;}
 void Lesson::setLecturer(Lecturer* lessonLecturer) {lecturer = lessonLecturer;}
 void Lesson::setBook(Book* lessonBook) {book = lessonBook;}
 void Lesson::setClassroom(Classroom* lessonClassroom) {classroom = lessonClassroom;}
 
-
+string Lesson::getSubject() const {return subject;}
 Lecturer* Lesson::getLecturer() const {return lecturer;}
 Book* Lesson::getBook() const {return book;}
 Classroom* Lesson::getClassroom() const {return classroom;}
--- lesson.hpp
+++ lesson.hpp
@@ -7,19 +7,22 @@ using std::string;
 
 class Lesson{
 private:
+    string name;
     string subject;
     Lecturer* lecturer;
     Book* book;
     Classroom* classroom; 
 
 public:
-    Lesson(const string& subject);
-    string getSubjectName() const;
-
+    Lesson(const string& name);
+    string getName() const;
+    
+    void setSubject(string lessonSubject);
     void setLecturer(Lecturer* lessonLecturer);
     void setBook(Book* lessonBook);
     void setClassroom(Classroom* lessonClassroom);
 
+    string getSubject() const;
     Lecturer* getLecturer() const;
     Book* getBook() const;
     Classroom* getClassroom() const;
