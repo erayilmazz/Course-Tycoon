8810d1a24519103d2717a81f9c8078db117803ac
Eray <eray.yilmaz3@std.yeditepe.edu.tr>
2024-10-26 10:46:15 +0300

    book getAuthor and setAuthor added.

--- book.cpp
+++ book.cpp
@@ -1,8 +1,10 @@
 #include "book.hpp"
 
 Book::Book(const string& title, const string& topic)
-    :title(title), topic(topic){}
+    : title(title), topic(topic) {}
 
+void Book::setAuthor(Author* bookAuthor) {author = bookAuthor;}
+Author* Book::getAuthor() {return author;}
 void Book::setTitle(string titleName) {title = titleName;}
 string Book::getTitle() const {return title;}
 void Book::setTopic(string topicName) {topic = topicName;}
--- book.hpp
+++ book.hpp
@@ -1,16 +1,19 @@
 #pragma once
 #include <string>
 using std::string;
+class Author;
 
 class Book{
     protected:
         string title;
         string topic;
-        //add author here
+        Author* author;
     public:
         Book() = default;
         Book(const string& title, const string& topic);
 
+        void setAuthor(Author* bookAuthor);
+        Author* getAuthor();
         void setTitle(string titleName);
         string getTitle() const;
         void setTopic(string topicName);
